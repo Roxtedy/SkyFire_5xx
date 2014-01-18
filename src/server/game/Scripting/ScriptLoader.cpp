e98502ea44bb559d86340f65b22e7464db826d7c
Roxtedy <belikov2015@gmail.com>
2014-01-18 14:06:34 -0800

    Update ScriptLoader.cpp

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -42,6 +42,7 @@ void AddSC_quest_spell_scripts();
 void AddSC_item_spell_scripts();
 void AddSC_example_spell_scripts();
 void AddSC_holiday_spell_scripts();
+void AddSC_monk_spell_scripts();
 
 void AddSC_SmartSCripts();
 
@@ -731,6 +732,7 @@ void AddSpellScripts()
     AddSC_item_spell_scripts();
     AddSC_example_spell_scripts();
     AddSC_holiday_spell_scripts();
+    AddSC_monk_spell_scripts();
 }
 
 void AddCommandScripts()
